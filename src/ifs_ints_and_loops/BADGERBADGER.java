43e92f791b132d1ef4e5972bf5ad57a79a3a5f07
aktrooper9 <league@WTS14-4.attlocal.net>
2019-05-15 18:21:57 -0700

    BADGER BADGER
    :unamused: :

---
+++ src/ifs_ints_and_loops/BADGERBADGER.java
@@ -0,0 +1,14 @@
+package ifs_ints_and_loops;
+
+public class BADGERBADGER {
+	public static void main(String[] args) {
+		for (int o = 0; o < 3; o++) {
+			for (int i = 0; i < 13; i++) {
+				System.out.println("Badger");
+			}
+			for (int w = 0; w < 2; w++) {
+				System.out.println("Mushroom");
+			}
+		}
+	}
+}
---
+++ src/ifs_ints_and_loops/NewAges.java
@@ -0,0 +1,11 @@
+package ifs_ints_and_loops;
+
+public class NewAges {
+public static void main(String[] args) {
+
+	for(int i = 0; i < 12; i++) {
+		System.out.println(i);
+	}
+	}
+}
+
